1b295e3b8aa603e9a7f1f766e3dd1d112e00f86f
Candide <biuvip@163.com>
2022-04-12 11:24:23 +0800

    更新默认配置

--- models/user.go
+++ models/user.go
@@ -1,7 +1,7 @@
 package models
 
 type User struct {
-	Id int `json:"id" orm:"id"`
+	Id int `json:"id" orm:"id" orm:"id"`
 	OpenId string `json:"openid" orm:"openid"`
 	Phone string `json:"phone" orm:"phone"`
 	Image string `json:"image" orm:"image"`
