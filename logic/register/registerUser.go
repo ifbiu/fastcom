57d03894cb318c57bbb51afe1f94cab755450de5
Candide <biuvip@163.com>
2022-04-13 23:06:14 +0800

    添加用户加入时间

--- logic/register/registerUser.go
+++ logic/register/registerUser.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"github.com/astaxie/beego/orm"
 	"log"
+	"time"
 )
 
 
 func AddUserInfo(user *models.User) (bool,bool,error) {
 	o := orm.NewOrm()
 	var users []models.User
+	now := time.Now().Format("2006-01-02 15:04:05")
 	_,err := o.Raw("SELECT id FROM user WHERE openid =?", user.OpenId).QueryRows(&users)
 	if err != nil {
 		return true,false, err
@@ -18,7 +20,7 @@ func AddUserInfo(user *models.User) (bool,bool,error) {
 	if len(users) > 0 {
 		return false,true, nil
 	}
-	r,err := o.Raw("INSERT INTO user(openid, phone, image, sex, nickname) values (?,?,?,?,?)", user.OpenId, user.Phone,user.Image,user.Sex,user.NickName).Exec()
+	r,err := o.Raw("INSERT INTO user(openid, phone, image, sex, nickname, createtime) values (?,?,?,?,?,?)", user.OpenId, user.Phone,user.Image,user.Sex,user.NickName,now).Exec()
 	fmt.Println(r.LastInsertId())
 	if err != nil {
 		return true,false, err
