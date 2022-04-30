3c7c7d92091794c520fe69cb0d92da4a738215e8
Candide <biuvip@163.com>
2022-04-30 21:17:53 +0800

    用户信息加手机号

--- logic/organize/getUserInfo.go
+++ logic/organize/getUserInfo.go
@@ -8,13 +8,14 @@ type responseResult struct {
 	Image string `json:"image"`
 	Sex int `json:"sex"`
 	NickName string `json:"nickName"`
+	Phone string `json:"phone"`
 }
 
 func GetUserInfo(openid string) (interface{},error) {
 	o := orm.NewOrm()
 	var user responseResult
 
-	err := o.Raw("select nick_name,image,sex from user where openid=?",openid).QueryRow(&user)
+	err := o.Raw("select nick_name,image,sex,phone from user where openid=?",openid).QueryRow(&user)
 	if err != nil {
 		return nil,err
 	}
