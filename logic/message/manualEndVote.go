51c0ef7f9e32c9a85b126cfac45990d584faf64c
Candide <biuvip@163.com>
2022-05-18 13:06:30 +0800

    投票结果列表

--- logic/message/manualEndVote.go
+++ logic/message/manualEndVote.go
@@ -2,8 +2,11 @@ package message
 
 import (
 	"errors"
+	"fastcom/db"
 	"fmt"
 	"github.com/astaxie/beego/orm"
+	"github.com/streadway/amqp"
+	"log"
 	"strconv"
 )
 
@@ -11,6 +14,7 @@ import (
 
 func ManualEndVote(openId string,typeId int) (error) {
 	o := orm.NewOrm()
+	var members []string
 	_, err := o.Raw("UPDATE vote SET is_end=3,manual_user=?,manual_time=now() WHERE id=?", openId,typeId).Exec()
 	if err != nil {
 		return err
@@ -43,5 +47,74 @@ func ManualEndVote(openId string,typeId int) (error) {
 			return err
 		}
 	}
+	_,err = o.Raw("SELECT DISTINCT openid FROM vote_success WHERE vote_item_id<>0").QueryRows(&members)
+	if err != nil {
+		return err
+	}
+	err = publishVoteResult(members)
+	if err != nil {
+		return err
+	}
+	for _, openid := range members {
+		_, err := o.Raw("INSERT INTO status (openid,organize_uuid,type,type_id,is_read,create_time) VALUES (?,(SELECT organize_uuid FROM vote WHERE id=?),4,?,1,now())",openid,typeId,typeId).Exec()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func publishVoteResult(members []string) error {
+	//topic := "topic:messageStr:"+uuidStr
+	conn, err := db.InitAmqp()
+	if err!=nil {
+		return err
+	}
+	// 创建信道
+	ch, err := conn.Channel()
+	if err!=nil {
+		return err
+	}
+	defer ch.Close()
+
+	//for _, openid := range openids {
+	//
+	//}
+	// 声明交换机
+	q, err := ch.QueueDeclare(
+		"fastcom", // 队列名字
+		false,   // 消息是否持久化
+		false,   // 不使用的时候删除队列
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	if err!=nil {
+		return err
+	}
+
+	openIdStr:=""
+	if len(members)>0 {
+		for i, openid := range members {
+			if i==0 {
+				openIdStr = openid
+			}else{
+				openIdStr = openIdStr + ","+openid
+			}
+		}
+	}
+
+	// 推送消息
+	err = ch.Publish(
+		"",     // exchange（交换机名字），这里忽略
+		q.Name, // 路由参数，这里使用队列名字作为路由参数
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing {
+			ContentType: "text/plain",
+			Body:        []byte(openIdStr),  // 消息内容
+		})
+
+	log.Printf("%s 发送内容",openIdStr)
 	return nil
 }
