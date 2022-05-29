e9b9ce7116bb0e0eab5f023cc0fb1fbd126d7a72
Candide <biuvip@163.com>
2022-05-29 08:55:57 +0800

    时间修改

--- logic/message/publishApprove.go
+++ logic/message/publishApprove.go
@@ -2,7 +2,6 @@ package message
 
 import (
 	"github.com/astaxie/beego/orm"
-	"time"
 )
 
 func SelectApprove(openid string,uuid string) (bool,error) {
@@ -32,8 +31,7 @@ func SelectApproveOpenIds(uuid string)([]string,error){
 
 func PublishApprove(openid string,openids []string,uuid string) (bool,error) {
 	o := orm.NewOrm()
-	now := time.Now().Format("2006-01-02 15:04:05")
-	exec, err := o.Raw("INSERT INTO approve (organize_uuid,start_user,create_time) VALUES (?,?,?)",uuid,openid,now).Exec()
+	exec, err := o.Raw("INSERT INTO approve (organize_uuid,start_user,create_time) VALUES (?,?,now())",uuid,openid).Exec()
 	if err != nil {
 		return false, err
 	}
@@ -41,8 +39,8 @@ func PublishApprove(openid string,openids []string,uuid string) (bool,error) {
 	if err != nil {
 		return false, err
 	}
-	for _, openid := range openids {
-		_, err := o.Raw("INSERT INTO status (openid,organize_uuid,type,type_id,is_read,create_time) VALUES (?,?,?,?,?,?)",openid,uuid,3,id,1,now).Exec()
+	for _, opid := range openids {
+		_, err := o.Raw("INSERT INTO status (openid,organize_uuid,type,type_id,is_read,create_time) VALUES (?,?,?,?,?,now())",opid,uuid,3,id,1).Exec()
 		if err != nil {
 			return false, err
 		}
