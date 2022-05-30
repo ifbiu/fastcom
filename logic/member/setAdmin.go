5662333462f0a13d9627732b784ba603555e44f6
Candide <biuvip@163.com>
2022-05-30 09:54:26 +0800

    事务提交

--- logic/member/setAdmin.go
+++ logic/member/setAdmin.go
@@ -7,10 +7,13 @@ import (
 
 func SetAdmin(uuid int,setOpenId string) (error) {
 	o := orm.NewOrm()
+	_ = o.Begin()
 	exec, err := o.Raw("UPDATE member SET authority=2 WHERE organize_uuid = ? AND openid= ?", uuid, setOpenId).Exec()
 	if err != nil {
+		_ = o.Rollback()
 		return err
 	}
+	_ = o.Commit()
 	affected, err := exec.RowsAffected()
 	if err != nil {
 		return err
--- logic/organize/addOrganize.go
+++ logic/organize/addOrganize.go
@@ -38,21 +38,27 @@ func GenerateUuid() (string,error) {
 
 func AddOrganize(uuid int,maximum int,openid string,organizeName string,coverImg string,introduce string,authorName string) (bool,error) {
 	o := orm.NewOrm()
+	_ = o.Begin()
 	now := time.Now().Format("2006-01-02 15:04:05")
 	r1,err := o.Raw("INSERT INTO organize(uuid, cover_img, organize_name, introduce,maximum, create_time) values (?,?,?,?,?,?)",uuid,coverImg,organizeName,introduce,maximum,now).Exec()
 	if err != nil {
+		_ = o.Rollback()
 		return false, err
 	}
 	organizeId, err := r1.LastInsertId()
 	if err != nil {
+		_ = o.Rollback()
 		return false, err
 	}
 	if organizeId == 0 {
+		_ = o.Rollback()
 		return false, err
 	}
 	_,err = o.Raw("INSERT INTO member(organize_uuid, name, openid,authority, create_time) values (?,?,?,?,?)",uuid,authorName,openid,1,now).Exec()
 	if err != nil {
+		_ = o.Rollback()
 		return false, err
 	}
+	_ = o.Commit()
 	return true, nil
 }
