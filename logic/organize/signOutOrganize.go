dd72ef8414e3360548634f7daae634142f359954
Candide <biuvip@163.com>
2022-05-30 10:37:14 +0800

    是否退出组织1

--- logic/organize/signOutOrganize.go
+++ logic/organize/signOutOrganize.go
@@ -4,16 +4,21 @@ import "github.com/astaxie/beego/orm"
 
 func SignOutOrganize(openId string,uuid int) (bool,error) {
 	o := orm.NewOrm()
-	r1,err := o.Raw("delete from member where organize_uuid=? and openid=?",uuid,openId).Exec()
+	_ = o.Begin()
+	r1,err := o.Raw("update member SET is_del=2 where organize_uuid=? and openid=?",uuid,openId).Exec()
 	if err != nil {
+		_ = o.Rollback()
 		return false, err
 	}
 	organizeId, err := r1.RowsAffected()
 	if err != nil {
+		_ = o.Rollback()
 		return false, err
 	}
 	if organizeId == 0 {
+		_ = o.Rollback()
 		return false, err
 	}
+	_ = o.Commit()
 	return true, nil
 }
