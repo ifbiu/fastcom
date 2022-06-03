cc1f765473c145d7b91b8d5f9e4c6390d4b4e9b2
Candide <biuvip@163.com>
2022-06-03 14:35:58 +0800

    时间问题修复

--- common/formatTime.go
+++ common/formatTime.go
@@ -4,13 +4,14 @@ import "time"
 
 func FormatTime(tempTime time.Time) (string) {
 	now := time.Now()
+	timeStr := now.Format("2006-01-02")
+	t, _ := time.ParseInLocation("2006-01-02 15:04:05", timeStr+" 23:59:59", time.Local)
 	if now.Year() == tempTime.Year() &&
 		now.Month() == tempTime.Month() &&
 		now.Day() == tempTime.Day() {
 		return tempTime.Format("15:04")
-	}else if now.Year() == tempTime.Year() &&
-		now.Month() == tempTime.Month() &&
-		now.Day()-1 == tempTime.Day() {
+	}else if tempTime.Unix() > t.Unix() + 1 - 86400- 86400 &&
+		tempTime.Unix() < t.Unix() - 86400{
 		return tempTime.Format("昨天 15:04")
 	}else if now.Year() == tempTime.Year(){
 		return tempTime.Format("01月02日")
