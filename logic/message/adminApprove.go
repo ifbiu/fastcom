da11b35dafae3091f1502af08adc1fcf1d07935d
Candide <biuvip@163.com>
2022-06-04 21:59:55 +0800

    组织销毁1

--- logic/message/adminApprove.go
+++ logic/message/adminApprove.go
@@ -13,7 +13,7 @@ func AdminApprove(openId string,typeId int,approve int) (error) {
 		return err
 	}
 	if approve==1 {
-		_ , err := o.Raw("INSERT INTO member (organize_uuid,name,openid,authority,create_time) VALUES((SELECT organize_uuid FROM approve WHERE id=?),(SELECT user.nick_name FROM approve left join user on approve.start_user=user.openid WHERE  approve.id=?),(SELECT start_user FROM approve WHERE id=?),3,now())", typeId,typeId,typeId).Exec()
+		_ , err := o.Raw("INSERT INTO member (organize_uuid,name,openid,authority,create_time,is_del) VALUES((SELECT organize_uuid FROM approve WHERE id=?),(SELECT user.nick_name FROM approve left join user on approve.start_user=user.openid WHERE  approve.id=?),(SELECT start_user FROM approve WHERE id=?),3,now(),1)", typeId,typeId,typeId).Exec()
 		if err != nil {
 			return err
 		}
